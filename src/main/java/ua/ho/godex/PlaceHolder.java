2e44dc0589516c7fa6d79f5b6beab7925cd595a8
PavlenkoB <godexxxx@gmail.com>
2017-08-24 00:11:43 +0300

    init

--- src/main/java/ua/ho/godex/PlaceHolder.java
+++ src/main/java/ua/ho/godex/PlaceHolder.java
@@ -1,16 +1,20 @@
 package ua.ho.godex;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Creator: Pavlenko Bohdan
  * Date: 23.08.2017
  * Project: istore
  */
+@SpringBootApplication
 public class PlaceHolder {
     static public Integer sum(Integer firsta, Integer second){
         return  firsta+second;
     }
 
     public static void main(String[] args) {
-        System.out.println("text");
+        SpringApplication.run(PlaceHolder.class, args);
     }
 }
