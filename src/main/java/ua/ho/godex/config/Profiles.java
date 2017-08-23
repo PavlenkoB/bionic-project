44bd80bd28e60bdcbeb57a7c44e6440ac4957f90
PavlenkoB <godexxxx@gmail.com>
2017-08-24 00:18:32 +0300

    crate base structure

---
+++ src/main/java/ua/ho/godex/config/Profiles.java
@@ -0,0 +1,10 @@
+package ua.ho.godex.config;
+
+public class Profiles {
+    public static final String
+            POSTGRES = "postgres",
+            HSQLDB = "hsqldb",
+            JDBC = "jdbc",
+            JPA = "jpa",
+            MOCK = "mock";
+}
---
+++ src/main/java/ua/ho/godex/service/impl/ProductServiceImpl.java
@@ -0,0 +1,48 @@
+package ua.ho.godex.service.impl;
+
+import ua.ho.godex.service.ProductService;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * Creator: Pavlenko Bohdan
+ * Date: 24.08.2017
+ * Project: supStore
+ */
+public class ProductServiceImpl implements ProductService {
+    @Override
+    public List<Product> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Product> getAll(String name, BigDecimal min, BigDecimal max, ProductSort productSort, int offset, int limit) {
+        return null;
+    }
+
+    @Override
+    public int getCount(String name, BigDecimal min, BigDecimal max) {
+        return 0;
+    }
+
+    @Override
+    public Product getById(int productId) {
+        return null;
+    }
+
+    @Override
+    public Product create(Product product) {
+        return null;
+    }
+
+    @Override
+    public void update(Product product) {
+
+    }
+
+    @Override
+    public void delete(Integer productId) {
+
+    }
+}
