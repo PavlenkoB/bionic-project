1dc36f395c10c38b4a245fe0b999945f71cc91d2
PavlenkoBohdan <povlenkobohdan@gmail.com>
2017-08-25 18:18:58 +0300

    update db structure create product domain object update create hsql script

--- src/main/java/ua/ho/godex/domain/Product.java
+++ src/main/java/ua/ho/godex/domain/Product.java
@@ -10,5 +10,41 @@ public class Product {
     @Access(AccessType.PROPERTY)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    private String name;
+    private String description;
+    @Column(name = "category_id")
+    private Integer categoryId;
 
+    public Product() {
+    }
+
+    public Product(String name, String description, Integer categoryId) {
+        this.name = name;
+        this.description = description;
+        this.categoryId = categoryId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
