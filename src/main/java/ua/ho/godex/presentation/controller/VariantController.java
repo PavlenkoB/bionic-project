29f6ff9b9ded7fe36add3231cf724e8aa0fb6e60
PavlenkoB <godexxxx@gmail.com>
2017-09-02 20:06:55 +0300

    write User Controller

---
+++ src/main/java/ua/ho/godex/presentation/controller/VariantController.java
@@ -0,0 +1,29 @@
+package ua.ho.godex.presentation.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ua.ho.godex.domain.Variant;
+import ua.ho.godex.service.VariantService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("variants")
+public class VariantController {
+    final private VariantService variantService;
+
+    @Autowired
+    public VariantController(VariantService variantService) {
+        this.variantService = variantService;
+    }
+
+    @GetMapping
+    public String showVariants(Model model) {
+        List<Variant> variantList = variantService.getAll();
+        model.addAttribute("variants", variantList);
+        return "/variants/variants-list";
+    }
+}
