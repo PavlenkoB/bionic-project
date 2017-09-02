091c37663d4a59f7f2cb5315958babed5c7c2aa6
PavlenkoB <godexxxx@gmail.com>
2017-09-02 11:30:52 +0300

    add gulp

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # bionic-project
-bionic exsamination project
+This is examination project
+
+##Used utils
+ * **npm** npm install for get utils for gulp
+ * **gulp** file 'gulpfile.js' for recompile less to css and minimize
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 'use strict';
 
-var gulp = require('gulp');
-var less = require('gulp-less');
-var minify = require('gulp-minify-css');
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    minify = require('gulp-minify-css'),
+    livereload = require('gulp-livereload');
 
 gulp.task('less', function () {
     return gulp.src('./src/main/webapp/WEB-INF/web-resources/less/*.less')
@@ -12,9 +13,16 @@ gulp.task('less', function () {
             ]
         }))
         .pipe(minify({processImport: false}))
-        .pipe(gulp.dest('./src/main/webapp/WEB-INF/web-resources/css/'));
+        .pipe(gulp.dest('./src/main/webapp/WEB-INF/web-resources/css/'))
+        .pipe(livereload());
 });
-
+gulp.task('watch', function () {
+    livereload.listen();
+    gulp.watch(
+        './src/main/webapp/WEB-INF/web-resources/less/*.less',
+        ['less']
+    );
+});
 gulp.task('build', ['less']);
-gulp.task('default', ['less']);
+gulp.task('default', ['less', 'watch']);
 //todo use Gulp
